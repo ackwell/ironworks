fae8fa4ac38fbbc35220ae3810d8432099d5d418
Saxon Landers <saxon@ackwell.com.au>
2022-02-21 18:08:30 +1100

    Scaffold juniper quick start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use juniper::graphql_value;
+
+#[derive(juniper::GraphQLEnum, Clone, Copy)]
+enum Episode {
+	NewHope,
+	Empire,
+	Jedi,
+}
+
+struct Context(Episode);
+impl juniper::Context for Context {}
+
+struct Query;
+#[juniper::graphql_object(context = Context)]
+impl Query {
+	fn favouriteEpisode(context: &Context) -> juniper::FieldResult<Episode> {
+		return Ok(context.0);
+	}
+}
+
+type Schema = juniper::RootNode<'static, Query, juniper::EmptyMutation<Context>, juniper::EmptySubscription<Context>>;
+
 fn main() {
-	println!("Hello, world!");
+	let context = Context(Episode::NewHope);
+
+	let (res, _errors) = juniper::execute_sync(
+		"query { favouriteEpisode }", 
+		None, 
+		&Schema::new(Query, juniper::EmptyMutation::new(), juniper::EmptySubscription::new()), 
+		&juniper::Variables::new(), 
+		&context,
+	)
+		.unwrap();
+
+	assert_eq!(
+		res,
+		graphql_value!({ "favouriteEpisode": "NEW_HOPE" })
+	)
 }
