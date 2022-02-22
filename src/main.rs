ef61150239d4d6c132c82b7b43522f9933b9d899
Saxon Landers <saxon@ackwell.com.au>
2022-02-22 11:57:11 +1100

    Implement GraphQLType manually

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,59 @@ enum Episode {
 struct Context(Episode);
 impl juniper::Context for Context {}
 
-struct Query;
-#[juniper::graphql_object(context = Context)]
-impl Query {
-	fn favouriteEpisode(context: &Context) -> juniper::FieldResult<Episode> {
-		return Ok(context.0);
+struct Query {}
+
+impl juniper::GraphQLType<juniper::DefaultScalarValue> for Query {
+	fn name(_info: &()) -> Option<&'static str> {
+		return Some("todo");
+	}
+
+	fn meta<'registry>(_info: &(), registry: &mut juniper::Registry<'registry>) -> juniper::meta::MetaType<'registry>
+	where juniper::DefaultScalarValue: 'registry
+	{
+		let fields = &[
+			registry.field::<&Episode>("favouriteEpisode", &()),
+		];
+
+		return registry
+			.build_object_type::<Query>(&(), fields)
+			.into_meta();
+	}
+}
+
+impl juniper::GraphQLValue for Query {
+	type Context = Context;
+	type TypeInfo = ();
+
+	fn type_name(&self, _info: &()) -> Option<&'static str> {
+		return <Query as juniper::GraphQLType>::name(&());
+	}
+
+	fn resolve_field(
+		&self,
+		info: &(),
+		_field_name: &str,
+		_args: &juniper::Arguments,
+		executor: &juniper::Executor<Context>
+	) -> juniper::ExecutionResult {
+		let context = executor.context();
+		return executor.resolve_with_ctx(info, &context.0);
 	}
 }
 
 fn main() {
+	let query = Query {};
+
 	let schema: juniper::RootNode<Query, juniper::EmptyMutation<Context>, juniper::EmptySubscription<Context>> = juniper::RootNode::new_with_info(
-		Query,
+		query,
 		juniper::EmptyMutation::new(),
 		juniper::EmptySubscription::new(),
 		(),
 		(),
 		(),
 	);
+	
+	// ---
 
 	let context = Context(Episode::NewHope);
 
