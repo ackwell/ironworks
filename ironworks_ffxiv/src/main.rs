2854194dd142115829f01628f386f81be7d7bfb0
ackwell <saxon@ackwell.com.au>
2022-03-04 20:40:01 +1100

    Implement installaction location finder

--- ironworks_ffxiv/src/main.rs
+++ ironworks_ffxiv/src/main.rs
@@ -2,23 +2,41 @@
 
 #![allow(clippy::needless_return)]
 
-use std::{error::Error, path::PathBuf};
+use std::{error::Error, fs, path::PathBuf};
 
 use ironworks_sqpack::{Category, Repository, SqPack};
 
+const TRY_PATHS: [&str; 5] = [
+	"C:\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn",
+	"C:\\Program Files (x86)\\Steam\\steamapps\\common\\FINAL FANTASY XIV Online",
+	"C:\\Program Files (x86)\\Steam\\steamapps\\common\\FINAL FANTASY XIV - A Realm Reborn",
+	"C:\\Program Files (x86)\\FINAL FANTASY XIV - A Realm Reborn",
+	"C:\\Program Files (x86)\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn",
+];
+
+const WSL_PREFIX: [&str; 2] = ["/mnt", "c"];
+
+const SQPACK_PATH: [&str; 2] = ["game", "sqpack"];
+
 fn main() -> Result<(), Box<dyn Error>> {
-	let sqpack = SqPack::new(String::from("ffxiv"), [
-		Repository {
+	// TODO: allow override
+	let mut install = find_install().unwrap();
+	for segment in SQPACK_PATH {
+		install.push(segment);
+	}
+
+	let sqpack = SqPack::new(
+		String::from("ffxiv"),
+		[Repository {
 			id: 0,
 			name: String::from("ffxiv"),
-			path: PathBuf::from("/mnt/c/Program Files (x86)/SquareEnix/FINAL FANTASY XIV - A Realm Reborn/game/sqpack/ffxiv")
-		}
-	], [
-		Category {
+			path: install.join("ffxiv"),
+		}],
+		[Category {
 			id: 0x0A,
 			name: String::from("exd"),
-		}
-	]);
+		}],
+	);
 
 	let file_buffer = sqpack.read_file("exd/root.exl")?;
 
@@ -27,4 +45,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 	println!("EXLT: {}", exlt);
 
 	return Ok(());
+}
+
+fn find_install() -> Option<PathBuf> {
+	return TRY_PATHS
+		.iter()
+		.flat_map(|path| {
+			[
+				PathBuf::from(path),
+				WSL_PREFIX
+					.into_iter()
+					.chain(path.split('\\').skip(1))
+					.collect::<PathBuf>(),
+			]
+		})
+		.find(|path| fs::metadata(path).is_ok())
+		.map(PathBuf::from);
 }
