bc11d845ef6c0cb3a5fa85035b985f2a9e1267af
ackwell <saxon@ackwell.com.au>
2022-03-21 23:30:02 +1100

    Flesh out rudimentary node structure

--- ironworks_schema/src/lib.rs
+++ ironworks_schema/src/lib.rs
@@ -1,8 +1,20 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
+use std::collections::HashMap;
+
+// enum_dispatch?
+
+pub enum Node {
+	Array(ArrayNode),
+	Scalar(ScalarNode),
+	Struct(StructNode),
+}
+
+pub struct ArrayNode {
+	node: Box<Node>,
+	count: u32,
+}
+
+pub struct ScalarNode {}
+
+pub struct StructNode {
+	nodes: HashMap<String, Node>,
 }
