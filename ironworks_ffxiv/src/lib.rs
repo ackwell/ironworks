612745b1be1da1631e1f83fa138e4e4bd6132c40
ackwell <saxon@ackwell.com.au>
2022-03-21 19:04:34 +1100

    Add schema crate scaffold

--- ironworks_ffxiv/src/lib.rs
+++ ironworks_ffxiv/src/lib.rs
@@ -4,6 +4,8 @@ Collection of helper for working with FFXIV data utilising the Ironworks toolkit
 
 #![warn(missing_debug_implementations, missing_docs)]
 
+// TODO: is it honestly worth seperating xiv stuff? really?
+
 mod excel;
 mod sqpack;
 
