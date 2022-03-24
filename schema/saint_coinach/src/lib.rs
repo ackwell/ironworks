c3c5536cdc62ad4e032fb7af3d1d07a56a138b9f
ackwell <saxon@ackwell.com.au>
2022-03-24 19:15:59 +1100

    Scaffold repository construction

--- schema/saint_coinach/src/lib.rs
+++ schema/saint_coinach/src/lib.rs
@@ -1,27 +1,102 @@
-use std::env::current_exe;
+use std::{
+	env::current_exe,
+	path::{Path, PathBuf},
+};
 
 use git2::{build::RepoBuilder, Repository};
 
-pub fn test() {
-	// todo error handling lmao
-	let repository_path = current_exe()
-		.unwrap()
-		.parent()
-		.unwrap()
-		.join("__test_coinach_repo");
+// need to build some trait that represents what a "schema provider" looks like (ref manacutter probably)
+// impl of that trait for stc can probably own a repository ref and do lazy lookups into the object db
+// given how git works a canonical version is easy to trip a "need to update" check but will need to consider how to trip an update check for a ref like "HEAD"
 
-	let repository = repository_path
-		.exists()
-		.then(|| Repository::open_bare(&repository_path))
-		.unwrap_or_else(|| {
-			RepoBuilder::new().bare(true).clone(
-				"https://github.com/xivapi/SaintCoinach.git",
-				&repository_path,
-			)
-		});
+// Default configuration
+const REPOSITORY_URL: &str = "https://github.com/xivapi/SaintCoinach.git";
+const REPOSITORY_DIRECTORY: &str = "saint_coinach";
 
-	match repository {
-		Err(e) => panic!("{}", e),
-		Ok(repo) => println!("yay:{}", repo.head().unwrap().name().unwrap()),
+// todo: name?
+#[derive(Debug)]
+struct SaintCoinachSchemaOptions {
+	remote: Option<String>,
+	directory: Option<PathBuf>,
+}
+
+impl SaintCoinachSchemaOptions {
+	fn new() -> Self {
+		SaintCoinachSchemaOptions {
+			remote: None,
+			directory: None,
+		}
 	}
+
+	fn remote(&mut self, remote: impl ToString) -> &mut Self {
+		self.remote = Some(remote.to_string());
+		self
+	}
+
+	fn directory(&mut self, directory: impl Into<PathBuf>) -> &mut Self {
+		self.directory = Some(directory.into());
+		self
+	}
+
+	#[inline]
+	fn build(&self) -> SaintCoinachSchema {
+		SaintCoinachSchema::with_options(self)
+	}
+}
+
+// TODO: can't derive debug on this due to repo - look into crates like `derivative` to handle?
+struct SaintCoinachSchema {
+	repository: Repository,
+}
+
+impl SaintCoinachSchema {
+	#[inline]
+	fn new() -> Self {
+		Self::with_options(&Self::options())
+	}
+
+	#[inline]
+	fn options() -> SaintCoinachSchemaOptions {
+		SaintCoinachSchemaOptions::new()
+	}
+
+	fn with_options(options: &SaintCoinachSchemaOptions) -> Self {
+		// todo: look into fs::canonicalize but it sounds like it only works for pre-existing stuff
+		let directory = options.directory.clone().or_else(default_directory);
+
+		// TODO: handle error
+		let directory =
+			directory.expect("dir not found or not provided or something fucky went wrong pls fix");
+
+		let remote = options
+			.remote
+			.clone()
+			.unwrap_or_else(|| REPOSITORY_URL.to_string());
+
+		let repository = if directory.exists() {
+			Repository::open_bare(directory)
+		} else {
+			RepoBuilder::new().bare(true).clone(&remote, &directory)
+		};
+
+		// TODO: handle error
+		let repository = repository.unwrap();
+
+		Self { repository }
+	}
+}
+
+fn default_directory() -> Option<PathBuf> {
+	match current_exe() {
+		Ok(path) => path
+			.parent()
+			.map(|parent| parent.join(REPOSITORY_DIRECTORY)),
+		Err(_) => None,
+	}
+}
+
+pub fn test() {
+	let schema = SaintCoinachSchema::new();
+
+	println!("yay:{}", schema.repository.head().unwrap().name().unwrap())
 }
