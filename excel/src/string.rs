9d170ae35559cf2ce3b9d73362cb5e7fd8549126
ackwell <saxon@ackwell.com.au>
2022-04-08 21:36:48 +1000

    Remove old implementations

--- excel/src/string.rs
+++
@@ -1,8 +0,0 @@
-use binrw::{BinRead, NullString};
-
-#[derive(BinRead, Debug)]
-pub struct SeString {
-	// TODO: Probably should store this as a byte vec with utils
-	#[allow(dead_code)]
-	raw: NullString,
-}
