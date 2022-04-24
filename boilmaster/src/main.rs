8f82b52194b537224ba2dc7be32f78f43f8423f1
ackwell <saxon@ackwell.com.au>
2022-04-25 03:09:18 +1000

    axum boilerplate

--- boilmaster/src/main.rs
+++ boilmaster/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-	println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Router, Server};
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new().route("/", get(test));
+
+	Server::bind(&SocketAddr::from(([0, 0, 0, 0], 8080)))
+		.serve(app.into_make_service())
+		.await
+		.unwrap()
+}
+
+async fn test() -> impl IntoResponse {
+	"Hello world."
 }
