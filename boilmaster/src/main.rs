4ef3eb01989d98a27d367e00ec3079f79a6d79b2
ackwell <saxon@ackwell.com.au>
2022-04-25 23:33:23 +1000

    Add graceful shutdown signal

--- boilmaster/src/main.rs
+++ boilmaster/src/main.rs
@@ -2,6 +2,7 @@ use std::net::SocketAddr;
 
 use axum::Server;
 use boilmaster::http;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -10,6 +11,33 @@ async fn main() {
 	// TODO: not sure if this should be here or http. think about it.
 	Server::bind(&SocketAddr::from(([0, 0, 0, 0], 8080)))
 		.serve(app.into_make_service())
+		.with_graceful_shutdown(shutdown_signal())
 		.await
 		.unwrap()
+}
+
+async fn shutdown_signal() {
+	let ctrl_c = async {
+		signal::ctrl_c()
+			.await
+			.expect("Failed to install Ctrl+C handler.");
+	};
+
+	#[cfg(unix)]
+	let terminate = async {
+		signal::unix::signal(signal::unix::SignalKind::terminate())
+			.expect("Failed to install SIGTERM handler.")
+			.recv()
+			.await
+	};
+
+	#[cfg(not(unix))]
+	let terminate = std::future::pending::<()>();
+
+	tokio::select! {
+		_ = ctrl_c => {},
+		_ = terminate => {},
+	}
+
+	println!("Shutdown signal received.")
 }
