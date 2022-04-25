4856c53664b6b5bef315bff33a7114fc61111f01
ackwell <saxon@ackwell.com.au>
2022-04-25 15:24:36 +1000

    Set up rudimentary sheet list endpoint

--- boilmaster/src/http.rs
+++ boilmaster/src/http.rs
@@ -1,9 +1,19 @@
+use std::io::Read;
+
 use axum::{response::IntoResponse, routing::get, Router};
+use ironworks::{ffxiv, sqpack::SqPack};
 
 pub fn router() -> Router {
-	Router::new().route("/", get(hello_world))
+	Router::new().route("/sheets", get(sheets))
 }
 
-async fn hello_world() -> impl IntoResponse {
-	"Hello world."
+async fn sheets() -> impl IntoResponse {
+	// TODO this should not be done every request
+	// TODO probably need something in iw::excel for listing sheet names publicly
+	let sqpack = SqPack::new(ffxiv::FsResource::search().unwrap());
+	let mut root = sqpack.file("exd/root.exl").unwrap();
+	let mut string = String::new();
+	root.read_to_string(&mut string).unwrap();
+
+	string
 }
--- boilmaster/src/main.rs
+++ boilmaster/src/main.rs
@@ -7,6 +7,7 @@ use boilmaster::http;
 async fn main() {
 	let app = http::router();
 
+	// TODO: not sure if this should be here or http. think about it.
 	Server::bind(&SocketAddr::from(([0, 0, 0, 0], 8080)))
 		.serve(app.into_make_service())
 		.await
