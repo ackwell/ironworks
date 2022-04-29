ca48ff8a9be1468410ac9eaf1a4cb1a6b25df082
ackwell <saxon@ackwell.com.au>
2022-04-30 02:20:10 +1000

    Add trait for file reading

--- ironworks/src/ironworks.rs
+++ ironworks/src/ironworks.rs
@@ -7,11 +7,23 @@ pub trait Provider: 'static {
 	fn file(&self, path: &str) -> Result<Vec<u8>>;
 }
 
+pub trait File {
+	// might need an error type?
+	fn read(data: Vec<u8>) -> Self;
+}
+
+impl File for Vec<u8> {
+	fn read(data: Vec<u8>) -> Self {
+		data
+	}
+}
+
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct Ironworks {
 	#[derivative(Debug = "ignore")]
 	providers: Vec<Box<dyn Provider>>,
+	// todo: does this own the file cache, then?
 }
 
 impl Default for Ironworks {
@@ -35,14 +47,17 @@ impl Ironworks {
 
 	// todo version?
 
-	pub fn file(&self, path: &str) -> Result<Vec<u8>> {
-		self.providers
+	pub fn file<F: File>(&self, path: &str) -> Result<F> {
+		let data = self
+			.providers
 			.iter()
 			.rev()
 			.map(|provider| provider.file(path))
 			// todo check the notfound type?
 			.find(|result| !matches!(result, Err(Error::NotFound(_))))
 			// TODO: this should be a "path" errorvalue and filepath/sqpackpath can probs be removed
-			.unwrap_or_else(|| Err(Error::NotFound(ErrorValue::Other(path.into()))))
+			.unwrap_or_else(|| Err(Error::NotFound(ErrorValue::Other(path.into()))))?;
+
+		Ok(F::read(data))
 	}
 }
