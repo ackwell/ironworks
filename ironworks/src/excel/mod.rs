141c5f4ace193b7b91d2d29464c99e16cc878f1d
ackwell <saxon@ackwell.com.au>
2022-04-30 21:52:21 +1000

    Remove old excel resource

--- ironworks/src/excel/mod.rs
+++ ironworks/src/excel/mod.rs
@@ -4,13 +4,11 @@ mod excel;
 mod excel_options;
 mod mapper;
 mod metadata;
-mod resource;
 mod sheet;
 
 pub use {
 	excel::Excel,
 	mapper::Mapper,
 	metadata::SheetMetadata,
-	resource::Resource,
 	sheet::{Column, ColumnKind, Field, Row, RowOptions, Sheet},
 };
--- ironworks/src/ffxiv/mod.rs
+++ ironworks/src/ffxiv/mod.rs
@@ -1,13 +1,8 @@
 //! FFXIV specialisations and utilities for ironworks.
 
 mod fs;
-
 mod mapper;
-#[cfg(feature = "sqpack")]
-mod sqpack;
 
-#[cfg(feature = "sqpack")]
-pub use sqpack::SqPackResource;
 pub use {
 	fs::FsResource,
 	mapper::{Language, Mapper},
