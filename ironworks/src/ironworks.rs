0f240ab45fa692ff73dfc65b73a0c4c998cb6ab6
ackwell <saxon@ackwell.com.au>
2022-05-03 00:28:22 +1000

    Add ironworks docs

--- ironworks/src/ironworks.rs
+++ ironworks/src/ironworks.rs
@@ -6,11 +6,20 @@ use crate::{
 };
 
 // TODO: This shares name with sqpack::resource. conceptually it's similar but also kinda not. thoughts?
+/// Resource layer that can provide data to an ironworks instance.
 pub trait Resource: 'static {
+	/// Get the version string for the file at `path`. A return value of
+	/// `Err(Error::NotFound(ErrorValue::Path(_)))` will result in lookups
+	/// continuing to the next resource.
 	fn version(&self, path: &str) -> Result<String>;
+
+	/// Get the raw byte data for the file at `path`. A return value of
+	/// `Err(Error::NotFound(ErrorValue::Path(_)))` will result in lookups
+	/// continuing to the next resource.
 	fn file(&self, path: &str) -> Result<Vec<u8>>;
 }
 
+/// Core ironworks struct. Add one or more resources to query files.
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct Ironworks {
@@ -26,22 +35,28 @@ impl Default for Ironworks {
 }
 
 impl Ironworks {
+	/// Build a new instance of ironworks.
 	pub fn new() -> Self {
 		Self {
 			providers: Default::default(),
 		}
 	}
 
-	// todo note about .rev()
+	/// Add a resource to search for files. Resources are searched last-first; the
+	/// last resource added to ironworks that provides a requested path will be
+	/// the resource that is utilised.
 	pub fn resource(mut self, provider: impl Resource) -> Self {
 		self.providers.push(Box::new(provider));
 		self
 	}
 
+	/// Get the version string for the file at `path`.
 	pub fn version(&self, path: &str) -> Result<String> {
 		self.find_first(path, |provider, path| provider.version(path))
 	}
 
+	/// Read the file at `path`, using file type F to parse. To retrieve the file
+	/// as raw bytes, pass `Vec<u8>` to F.
 	pub fn file<F: File>(&self, path: &str) -> Result<F> {
 		let data = self.find_first(path, |provider, path| provider.file(path))?;
 		F::read(data)
