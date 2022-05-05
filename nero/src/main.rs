32485054bc74d1a94b94daff59a466d74bed1b3a
ackwell <saxon@ackwell.com.au>
2022-05-05 19:53:08 +1000

    Render sprite

--- nero/src/main.rs
+++ nero/src/main.rs
@@ -8,8 +8,18 @@ fn main() {
 		.add_plugin(EguiPlugin)
 		.insert_resource(WinitSettings::desktop_app())
 		.add_system(system_ui)
+		// View
+		.add_startup_system(startup_test)
 		// Done
 		.run();
+}
+
+fn startup_test(mut commands: Commands, asset_server: Res<AssetServer>) {
+	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+	commands.spawn_bundle(SpriteBundle {
+		texture: asset_server.load("icon.png"),
+		..default()
+	});
 }
 
 fn system_ui(mut egui_context: ResMut<EguiContext>) {
