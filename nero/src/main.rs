3133046b3b12a9f4ade834b59be65ddc41d8edbd
ackwell <saxon@ackwell.com.au>
2022-05-07 14:44:15 +1000

    Remove WASM handling

--- nero/src/main.rs
+++ nero/src/main.rs
@@ -1,8 +1,5 @@
 use bevy::{prelude::*, winit::WinitSettings};
 use bevy_egui::{egui, EguiContext, EguiPlugin};
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{Event, File, HtmlInputElement};
 
 fn main() {
 	App::new()
@@ -43,18 +40,6 @@ fn system_ui(
 		});
 }
 
-// web file picker testing
-// TODO: web stuff should be target gated
-// ????
-#[wasm_bindgen]
-extern "C" {
-	// TODO: name?
-	#[wasm_bindgen(extends=File, js_name=File, typescript_type="File")]
-	pub type File2;
-	#[wasm_bindgen(structural, method, getter, js_class="File", js_name=webkitRelativePath)]
-	pub fn webkit_relative_path(this: &File2) -> String;
-}
-
 // TODO: name
 struct EventRequestSqpack;
 
@@ -62,43 +47,6 @@ struct EventRequestSqpack;
 //       - looks like i can "chain" systems with In() and such to make an "error handling system"?
 fn system_request_sqpack(mut events: EventReader<EventRequestSqpack>) {
 	for _event in events.iter() {
-		request_directory().unwrap();
-	}
-}
-
-// TODO: name
-fn request_directory() -> Result<(), JsValue> {
-	let document = web_sys::window().unwrap().document().unwrap();
-	let input = document
-		.create_element("input")?
-		.dyn_into::<HtmlInputElement>()?;
-	input.set_type("file");
-	input.set_webkitdirectory(true);
-
-	// TODO: avoid leaking one of these every time
-	let closure = Closure::wrap(Box::new(on_file_change) as Box<dyn FnMut(_)>);
-	input.add_event_listener_with_callback("change", closure.as_ref().unchecked_ref())?;
-	closure.forget();
-
-	input.click();
-
-	Ok(())
-}
-
-// TODO: error handling here - few unwraps here are entirely possible
-fn on_file_change(event: Event) {
-	info!("CHANGE");
-
-	let files = event
-		.target()
-		.unwrap()
-		.dyn_into::<HtmlInputElement>()
-		.unwrap()
-		.files()
-		.unwrap();
-	for i in 0..files.length() {
-		let f2 = files.get(i).unwrap().dyn_into::<File2>().unwrap();
-		let path = f2.webkit_relative_path();
-		info!("path: {path:?}");
+		// request_directory().unwrap();
 	}
 }
