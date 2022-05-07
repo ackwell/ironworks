5f3a28d377a0587257caf3aaf8394fb7da7f6fb1
ackwell <saxon@ackwell.com.au>
2022-05-08 01:13:50 +1000

    Proof of concept IW asset loading

--- nero/src/main.rs
+++ nero/src/main.rs
@@ -1,20 +1,43 @@
-use bevy::{prelude::*, winit::WinitSettings};
+use std::{
+	io,
+	path::{Path, PathBuf},
+	sync::{Arc, RwLock},
+};
+
+use bevy::{
+	asset::{AssetIo, AssetIoError, AssetLoader, BoxedFuture, LoadContext, LoadedAsset},
+	prelude::*,
+	reflect::TypeUuid,
+	winit::WinitSettings,
+};
 use bevy_egui::{egui, EguiContext, EguiPlugin};
+use ironworks::{
+	ffxiv,
+	file::{exl, File},
+	sqpack::SqPack,
+	Error, ErrorValue, Ironworks,
+};
 
 fn main() {
 	App::new()
-		.add_plugins(DefaultPlugins)
+		.add_plugins_with(DefaultPlugins, |group| {
+			group.add_before::<bevy::asset::AssetPlugin, _>(IronworksPlugin)
+		})
 		// UI
 		.add_plugin(EguiPlugin)
 		.insert_resource(WinitSettings::desktop_app())
 		.add_system(system_ui)
 		// View
 		.add_startup_system(startup_test)
-		// TEMP: web stuff - plugin?
-		.add_event::<EventRequestSqpack>()
-		.add_system(system_request_sqpack)
+		// Ironworks
+		// TODO: This is split across two locations - should make two plugins, one for the loader and one for.... everything else, I guess?
+		.add_asset::<List>()
 		// Done
 		.run();
+}
+
+struct TempTestRes {
+	handle: Handle<List>,
 }
 
 fn startup_test(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -23,30 +46,140 @@ fn startup_test(mut commands: Commands, asset_server: Res<AssetServer>) {
 		texture: asset_server.load("icon.png"),
 		..default()
 	});
+
+	// TODO: realistically this shouldn't be here. just using to test. should it be an entity?
+	commands.insert_resource(TempTestRes {
+		handle: asset_server.load("iw://exd/root.exl"),
+	})
 }
 
 fn system_ui(
-	mut request_sqpack: EventWriter<EventRequestSqpack>,
 	mut egui_context: ResMut<EguiContext>,
+	temp_test: Res<TempTestRes>,
+	lists: Res<Assets<List>>,
 ) {
 	// todo: better id
 	egui::SidePanel::left("main")
 		.resizable(true)
 		.show(egui_context.ctx_mut(), |ui| {
 			ui.heading("nero");
-			if ui.button("sqpack").clicked() {
-				request_sqpack.send(EventRequestSqpack)
+
+			if let Some(List(list)) = lists.get(&temp_test.handle) {
+				let text = list.iter().fold(String::new(), |mut acc, cur| {
+					acc.reserve(cur.len() + 1);
+					acc.push_str(&cur);
+					acc.push('\n');
+					acc
+				});
+				ui.label(text);
 			}
 		});
 }
 
-// TODO: name
-struct EventRequestSqpack;
+pub struct IronworksPlugin;
+impl Plugin for IronworksPlugin {
+	fn build(&self, app: &mut App) {
+		let task_pool = app
+			.world
+			.get_resource::<bevy::tasks::IoTaskPool>()
+			.expect("IoTaskPool resource not found")
+			.0
+			.clone();
 
-// TODO: error handling. should probably put in seperate fn from the main system and unwrap once
-//       - looks like i can "chain" systems with In() and such to make an "error handling system"?
-fn system_request_sqpack(mut events: EventReader<EventRequestSqpack>) {
-	for _event in events.iter() {
-		// request_directory().unwrap();
+		let default_io = bevy::asset::create_platform_default_asset_io(app);
+
+		let ironworks = Arc::new(RwLock::new(Ironworks::new()));
+
+		// TODO: Try this eagerly with search and otherwise defer to adding resource later with explicit path?
+		ironworks
+			.write()
+			.unwrap()
+			.add_resource(SqPack::new(ffxiv::FsResource::search().unwrap()));
+
+		let asset_io = IronworksAssetIo {
+			default_io,
+			ironworks,
+		};
+
+		app.insert_resource(AssetServer::new(asset_io, task_pool))
+			.init_asset_loader::<ListAssetLoader>();
+	}
+}
+
+struct IronworksAssetIo {
+	default_io: Box<dyn AssetIo>,
+
+	ironworks: Arc<RwLock<Ironworks>>,
+}
+
+impl AssetIo for IronworksAssetIo {
+	fn load_path<'a>(&'a self, path: &'a Path) -> BoxedFuture<'a, Result<Vec<u8>, AssetIoError>> {
+		if let Ok(ironworks_path) = path.strip_prefix("iw://") {
+			Box::pin(async move {
+				self.ironworks
+					.read()
+					.unwrap()
+					.file::<Vec<u8>>(&ironworks_path.to_string_lossy())
+					.map_err(|error| match error {
+						Error::NotFound(ErrorValue::Path(path)) => {
+							AssetIoError::NotFound(path.into())
+						}
+						other => AssetIoError::Io(io::Error::new(io::ErrorKind::Other, other)),
+					})
+			})
+		} else {
+			self.default_io.load_path(path)
+		}
+	}
+
+	// The below just pass through to the base asset io, is it worth handling dirs, or changes for penumbra style resources?
+
+	fn read_directory(
+		&self,
+		path: &std::path::Path,
+	) -> Result<Box<dyn Iterator<Item = PathBuf>>, AssetIoError> {
+		self.default_io.read_directory(path)
+	}
+
+	fn is_directory(&self, path: &Path) -> bool {
+		self.default_io.is_directory(path)
+	}
+
+	fn watch_path_for_changes(&self, path: &Path) -> Result<(), AssetIoError> {
+		self.default_io.watch_path_for_changes(path)
+	}
+
+	fn watch_for_changes(&self) -> Result<(), AssetIoError> {
+		self.default_io.watch_for_changes()
+	}
+}
+
+// ???
+#[derive(Debug, TypeUuid)]
+#[uuid = "3584bf2d-97c2-42a1-a2a2-858f8bc4840b"]
+struct List(exl::ExcelList);
+
+// ??? temp stuff to test it works
+#[derive(Default)]
+struct ListAssetLoader;
+
+impl AssetLoader for ListAssetLoader {
+	fn load<'a>(
+		&'a self,
+		bytes: &'a [u8],
+		load_context: &'a mut LoadContext,
+	) -> BoxedFuture<'a, Result<(), anyhow::Error>> {
+		Box::pin(async move {
+			// TODO: this is pretty wasteful - none of the readers except vecu8 need an owned copy. that said, i'm not sure how best to handle the vec case - do i blindly copy and lose the passthrough benefit for other consumers? am i able to abuse asref or into<cow to allow [u8]|vecu80>vecu8?
+			let list = exl::ExcelList::read(bytes.to_vec())?;
+
+			load_context.set_default_asset(LoadedAsset::new(List(list)));
+
+			Ok(())
+		})
+	}
+
+	fn extensions(&self) -> &[&str] {
+		&["exl"]
 	}
 }
