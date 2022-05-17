60b84b3ec683002361c7e8eddaf5b1989102ab0b
ackwell <saxon@ackwell.com.au>
2022-05-18 03:16:05 +1000

    Add resources to ironworks from file picker result

--- nero/src/ironworks/asset_io.rs
+++ nero/src/ironworks/asset_io.rs
@@ -18,12 +18,17 @@ pub struct IronworksAssetIoPlugin;
 
 impl Plugin for IronworksAssetIoPlugin {
 	fn build(&self, app: &mut App) {
+		// Build the core instance of ironworks and store it as a resource for later.
 		let ironworks = Arc::new(RwLock::new(Ironworks::new()));
+		app.insert_resource(ironworks.clone());
 
 		// Try to find a game install, skipping straight to ready if one was found.
-		let state = match /* ffxiv::FsResource::search() */None::<ffxiv::FsResource> {
-			Some(res) => {
-				ironworks.write().unwrap().add_resource(SqPack::new(res));
+		let state = match ffxiv::FsResource::search() {
+			Some(resource) => {
+				ironworks
+					.write()
+					.unwrap()
+					.add_resource(SqPack::new(resource));
 				IronworksState::Ready
 			}
 			None => IronworksState::ResourceRequired,
@@ -68,14 +73,32 @@ fn request_resource(mut commands: Commands, task_pool: Res<AsyncComputeTaskPool>
 	commands.spawn().insert(PathSelection(task));
 }
 
-fn poll_path_selection(mut commands: Commands, mut tasks: Query<(Entity, &mut PathSelection)>) {
+fn poll_path_selection(
+	mut commands: Commands,
+	mut tasks: Query<(Entity, &mut PathSelection)>,
+	ironworks: Res<Arc<RwLock<Ironworks>>>,
+) {
 	for (entity, mut task) in tasks.iter_mut() {
 		// Poll the task once to check if there's a response from the dialog.
 		if let Some(response) = future::block_on(future::poll_once(&mut task.0)) {
-			info!("GOT:{response:?}");
+			let state = match response {
+				// A path was selected, add it as a resource and mark ready.
+				// TODO: try to sanity check the path somehow? Wrong path will just blow up. Might be able to query if one of the .ver files exists?
+				Some(file_handle) => {
+					let resource = ffxiv::FsResource::at(file_handle.path());
+					ironworks
+						.write()
+						.unwrap()
+						.add_resource(SqPack::new(resource));
 
-			// TODO: actually do something
-			commands.insert_resource(NextState(IronworksState::ResourceRequired));
+					IronworksState::Ready
+				}
+
+				// Interaction cancelled, jump back to the base state.
+				None => IronworksState::ResourceRequired,
+			};
+
+			commands.insert_resource(NextState(state));
 
 			// The task was completed, remove the marker entity.
 			commands.entity(entity).despawn();
