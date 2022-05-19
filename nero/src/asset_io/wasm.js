920b63f0259559907caa1eae5dbd4fb97011863a
ackwell <saxon@ackwell.com.au>
2022-05-20 02:43:10 +1000

    Expand on file list ideas a little

--- nero/src/asset_io/wasm.js
+++ nero/src/asset_io/wasm.js
@@ -1,14 +1,27 @@
+const fileListKey = Symbol('fileInputKey')
+
 export function pick_folder() {
+  // If the file list already exists, use that instance.
+  // TODO: Work out how this interacts with re-selecting sqpack or incorrect folders or w/e.
+  let files = window[fileListKey]
+  if (files != null) {
+    return Promise.resolve(files)
+  }
+
+  // Build & extract a promise.
   let resolve
   const promise = new Promise((resolvePromise) => {
     resolve = resolvePromise
   })
 
+  // Set up a folder picker input.
   const input = document.createElement('input')
   input.type = 'file'
   input.webkitdirectory = true
-  input.addEventListener('change', event => {
-    resolve(event.target.files)
+  // TODO: Look into rejecting promise if action is cancelled, can handle result on rust side.
+  input.addEventListener('change', ({target: {files}}) => {
+    window[fileListKey] = files
+    resolve(files)
   })
 
   input.click()
--- nero/src/asset_io/wasm.rs
+++ nero/src/asset_io/wasm.rs
@@ -18,14 +18,17 @@ impl Plugin for WasmIronworksPlugin {
 	}
 }
 
-fn request_resource() {
+fn request_resource(mut commands: Commands) {
 	// TODO: This seems close to impossible to move out of this system.
 	//       Tempted to say the approach should be reconsidered a little, and approached from an assetio reference. If wasm provides its own assetio impl, it would be able to natively await in load file calls - ergo it would be able to handle picking the folder and getting the array, and only tossing the slice into a fake "resource" just before it's needed.
-	//       Not sure how this works with a split between the folder picker and the byte reading, though - storing the JsValue anywhere makes it !Send.
+	//       Doing the above would likely permit skipping a fair amount of the logic handled in the native impl - if the fake resource pops up the folder picker on first use, then a really basic file can be requested by the state change system (this one) to check if it works, and operate directly on handles.
 	let future = async move {
 		let something = pick_folder().await;
-		info!("{:?}, trying command", something);
+		info!("{:?}", something);
 	};
 
 	wasm_bindgen_futures::spawn_local(future);
+
+	// TEMP
+	commands.insert_resource(NextState(IronworksState::ResourceRequired))
 }
