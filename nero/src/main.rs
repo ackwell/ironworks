3d738bbbd98d85154e4053b035f1ebb453b682b6
ackwell <saxon@ackwell.com.au>
2022-05-23 02:11:57 +1000

    Move camera logic to seperate file

--- nero/src/main.rs
+++ nero/src/main.rs
@@ -2,22 +2,14 @@
 
 mod asset_io;
 mod asset_loaders;
+mod camera;
 
 use asset_io::{IronworksAssetIoPlugin, IronworksState};
 use asset_loaders::{IronworksPlugin, List};
-use bevy::{
-	input::mouse::{MouseMotion, MouseScrollUnit, MouseWheel},
-	prelude::*,
-	winit::WinitSettings,
-};
+use bevy::{prelude::*, winit::WinitSettings};
 use bevy_egui::{egui, EguiContext, EguiPlugin};
+use camera::CameraPlugin;
 use iyes_loopless::prelude::*;
-use smooth_bevy_cameras::{
-	controllers::orbit::{
-		ControlEvent, OrbitCameraBundle, OrbitCameraController, OrbitCameraPlugin,
-	},
-	LookTransformPlugin,
-};
 
 fn main() {
 	App::new()
@@ -32,77 +24,11 @@ fn main() {
 		.add_system(ui_need_ironworks_resource.run_not_in_state(IronworksState::Ready))
 		.add_system(ui_main.run_in_state(IronworksState::Ready))
 		// 3D
-		.add_plugin(LookTransformPlugin)
-		.add_plugin(OrbitCameraPlugin::new(true))
-		.add_startup_system(spawn_camera)
-		.add_system(camera_controls)
+		.add_plugin(CameraPlugin)
 		// Asset test stuff
 		.add_enter_system(IronworksState::Ready, asset_test)
 		// Done
 		.run();
-}
-
-fn spawn_camera(mut commands: Commands) {
-	commands.spawn_bundle(OrbitCameraBundle::new(
-		OrbitCameraController {
-			// Smoothing the camera does _not_ mesh with desktop update tick.
-			smoothing_weight: 0.0,
-			..Default::default()
-		},
-		PerspectiveCameraBundle::default(),
-		Vec3::new(2.0, 0.0, 8.0),
-		Vec3::ZERO,
-	));
-}
-
-// Slightly tweaked copy of the default controls from the library because I didn't like the control scheme.
-fn camera_controls(
-	mut control_events: EventWriter<ControlEvent>,
-	mut mouse_wheel_reader: EventReader<MouseWheel>,
-	mut mouse_motion_events: EventReader<MouseMotion>,
-	mouse_buttons: Res<Input<MouseButton>>,
-	controllers: Query<&OrbitCameraController>,
-) {
-	// Get the controller for the camera.
-	let controller = match controllers.iter().find(|controller| controller.enabled) {
-		Some(controller) => controller,
-		_ => return,
-	};
-
-	let OrbitCameraController {
-		mouse_rotate_sensitivity,
-		mouse_translate_sensitivity,
-		mouse_wheel_zoom_sensitivity,
-		pixels_per_line,
-		..
-	} = *controller;
-
-	// Build the full mouse movement delta.
-	let cursor_delta = mouse_motion_events
-		.iter()
-		.fold(Vec2::ZERO, |total, event| total + event.delta);
-
-	// LMB translates on current plane.
-	if mouse_buttons.pressed(MouseButton::Left) {
-		control_events.send(ControlEvent::TranslateTarget(
-			mouse_translate_sensitivity * cursor_delta,
-		));
-	}
-
-	// RMB orbits current target.
-	if mouse_buttons.pressed(MouseButton::Right) {
-		control_events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
-	}
-
-	// Mouse wheel zooms current target.
-	let zoom = mouse_wheel_reader.iter().fold(1.0, |total, event| {
-		let amount = match event.unit {
-			MouseScrollUnit::Line => event.y,
-			MouseScrollUnit::Pixel => event.y * pixels_per_line,
-		};
-		total * (1.0 - amount * mouse_wheel_zoom_sensitivity)
-	});
-	control_events.send(ControlEvent::Zoom(zoom));
 }
 
 struct TempTestRes {
