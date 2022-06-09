b3b86058ea3b9a5b92c840d1506f4804b9e59712
ackwell <saxon@ackwell.com.au>
2022-06-09 21:49:24 +1000

    Clean up + document .eqdp

--- ironworks/src/file/eqdp.rs
+++ ironworks/src/file/eqdp.rs
@@ -3,12 +3,12 @@
 use std::{borrow::Cow, fmt::Debug, io::Cursor};
 
 use binrw::{binread, until_eof, BinRead};
-use modular_bitfield::prelude::*;
 
 use crate::error::Result;
 
 use super::file::File;
 
+/// Metadata for equipment and accessory sets on a per-race basis.
 #[binread]
 #[br(little)]
 pub struct EquipmentDeformerParameter {
@@ -34,23 +34,24 @@ impl File for EquipmentDeformerParameter {
 }
 
 impl EquipmentDeformerParameter {
-	// u16?
-	pub fn entry(&self, set_id: u16) -> Entry {
-		let block_index = usize::try_from(set_id / self.block_size).unwrap();
+	/// Get metadata for for specified set ID.
+	pub fn set(&self, id: u16) -> Set {
+		// Sets are laid out sequentially - grab the index of the block it should reside in.
+		let block_index = usize::try_from(id / self.block_size).unwrap();
 		if block_index > self.block_offsets.len() {
 			return Default::default();
 		}
 
-		// A block offset of u16::MAX designates the block to be "compressed" - emitted from the file, and equivalent to null bytes.
+		// A block offset of u16::MAX designates the block to be "compressed" - omitted from the file, and equivalent to null bytes.
 		let block_offset = self.block_offsets[block_index];
 		if block_offset == u16::MAX {
 			return Default::default();
 		}
 
+		// Read the entry from the data block.
 		let mut cursor = Cursor::new(&self.data);
-		cursor.set_position((block_offset + set_id % self.block_size).into());
-		// should this unwrap?
-		Entry::read(&mut cursor).unwrap()
+		cursor.set_position((block_offset + id % self.block_size).into());
+		Set(bitfield::Set::read(&mut cursor).unwrap())
 	}
 }
 
@@ -66,32 +67,70 @@ impl Debug for EquipmentDeformerParameter {
 	}
 }
 
-#[bitfield]
-#[derive(BinRead, Debug, Default)]
-pub struct Entry {
-	head_ears: Field,
-	body_neck: Field,
-	hands_wrists: Field,
-	legs_ring_r: Field,
-	feet_ring_l: Field,
-	#[skip]
-	_reserved: B6,
+/// Metadata for a specific set.
+#[derive(Debug, Default)]
+pub struct Set(bitfield::Set);
+
+macro_rules! set_slot {
+	($name:ident, $field:ident) => {
+		#[doc = concat!("Metadata for the ", stringify!($name), " slot.")]
+		pub fn $name(&self) -> Slot {
+			Slot(self.0.$field())
+		}
+	};
 }
 
-// do i want this?
-// impl Entry {
-// 	fn head(&self) -> Field {
-// 		self.head_ears()
-// 	}
+impl Set {
+	set_slot!(head, head_ears);
+	set_slot!(body, body_neck);
+	set_slot!(hands, hands_wrists);
+	set_slot!(legs, legs_ring_r);
+	set_slot!(feet, feet_ring_l);
 
-// 	fn ears(&self) -> Field {
-// 		self.head_ears()
-// 	}
-// }
+	set_slot!(ears, head_ears);
+	set_slot!(neck, body_neck);
+	set_slot!(wrists, hands_wrists);
+	set_slot!(ring_right, legs_ring_r);
+	set_slot!(ring_left, feet_ring_l);
+}
 
-#[bitfield(bits = 2)]
-#[derive(BitfieldSpecifier, Debug)]
-pub struct Field {
-	material: bool,
-	model: bool,
+/// Metadata for a slot within a set.
+#[derive(Debug)]
+pub struct Slot(bitfield::Slot);
+
+impl Slot {
+	/// Whether there is a dedicated material for this (Race, Set, Slot).
+	pub fn material(&self) -> bool {
+		self.0.material()
+	}
+
+	/// Whether there is a dedicated model for this (Race, Set, Slot).
+	pub fn model(&self) -> bool {
+		self.0.model()
+	}
+}
+
+#[allow(dead_code, clippy::identity_op, clippy::unnecessary_cast)]
+mod bitfield {
+	use binrw::BinRead;
+	use modular_bitfield::prelude::*;
+
+	#[bitfield]
+	#[derive(BinRead, Debug, Default)]
+	pub struct Set {
+		pub head_ears: Slot,
+		pub body_neck: Slot,
+		pub hands_wrists: Slot,
+		pub legs_ring_r: Slot,
+		pub feet_ring_l: Slot,
+		#[skip]
+		_reserved: B6,
+	}
+
+	#[bitfield(bits = 2)]
+	#[derive(BitfieldSpecifier, Debug)]
+	pub struct Slot {
+		pub material: bool,
+		pub model: bool,
+	}
 }
