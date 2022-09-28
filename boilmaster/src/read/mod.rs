8c1640e3e5c924d6432e72e752f106a1ee1703ab
ackwell <saxon@ackwell.com.au>
2022-09-28 19:13:46 +1000

    Split read module

---
+++ boilmaster/src/read/mod.rs
@@ -0,0 +1,5 @@
+mod read;
+mod value;
+
+pub use read::read_sheet;
+pub use value::Value;
--- boilmaster/src/read/read.rs
+++ boilmaster/src/read/read.rs
@@ -2,37 +2,8 @@ use std::collections::HashMap;
 
 use ironworks::excel;
 use ironworks_schema as schema;
-use serde::{Serialize, Serializer};
 
-#[derive(Debug, Serialize)]
-#[serde(untagged)]
-pub enum Value {
-	Array(Vec<Value>),
-	// TODO: should references even exist in this enum, or should we eagerly resolve them? I'm tempted to say the latter.
-	#[serde(serialize_with = "serialize_scalar")]
-	Scalar(excel::Field),
-	Struct(HashMap<String, Value>),
-}
-
-// TODO: this is effectively just making up for serialize not being impl'd in iw::excel - should that be enabled under a feature or is it better to do over here as we are?
-fn serialize_scalar<S: Serializer>(field: &excel::Field, s: S) -> Result<S::Ok, S::Error> {
-	use excel::Field as F;
-
-	match field {
-		// TODO: more comprehensive sestring handling
-		F::String(se_string) => s.serialize_str(&se_string.to_string()),
-		F::Bool(value) => s.serialize_bool(*value),
-		F::I8(value) => s.serialize_i8(*value),
-		F::I16(value) => s.serialize_i16(*value),
-		F::I32(value) => s.serialize_i32(*value),
-		F::I64(value) => s.serialize_i64(*value),
-		F::U8(value) => s.serialize_u8(*value),
-		F::U16(value) => s.serialize_u16(*value),
-		F::U32(value) => s.serialize_u32(*value),
-		F::U64(value) => s.serialize_u64(*value),
-		F::F32(value) => s.serialize_f32(*value),
-	}
-}
+use super::value::Value;
 
 // TODO: need some representation of filtering for this, preferably that will be constructable from reference filters, gql queries, and a get request for rest
 pub fn read_sheet(sheet: &schema::Sheet, row: &excel::Row) -> anyhow::Result<Value> {
