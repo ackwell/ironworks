0325c35702937003efde773230a73eb915b12c7d
ackwell <saxon@ackwell.com.au>
2022-10-01 23:59:02 +1000

    Test full action table ingest + search

--- boilmaster/src/search.rs
+++ boilmaster/src/search.rs
@@ -6,9 +6,11 @@ use ironworks::{
 	file::exh,
 };
 use tantivy::{
+	collector::TopDocs,
 	directory::MmapDirectory,
-	schema::{self, Schema},
-	Document, Index, IndexSettings,
+	query::QueryParser,
+	schema::{self, FieldType, Schema},
+	Document, Index, IndexSettings, ReloadPolicy,
 };
 
 pub fn temp_test_search(excel: &Excel) -> Result<()> {
@@ -18,6 +20,7 @@ pub fn temp_test_search(excel: &Excel) -> Result<()> {
 	let index_name = format!("{version}/{sheet_name}");
 
 	// TODO: just using action as a test case
+	// TODO: can probably avoid fetching the sheet if not building
 	let sheet = excel.sheet(sheet_name)?;
 
 	// TODO: configurable directory, this shouldn't be touching current exe at all
@@ -54,45 +57,84 @@ pub fn temp_test_search(excel: &Excel) -> Result<()> {
 	tracing::info!("has docs: {has_docs}");
 
 	if has_docs {
-		// lmao
+		// lmao - test quering just... to see. to see if it works. i mean why wouldn't it but fuck me i want to spike this without refactoring the trash first. yay trash
+		let reader = index
+			.reader_builder()
+			// TODO: this is set to manual 'cus technically an index is never updated in this setup. is that sane? i dunno?
+			.reload_policy(ReloadPolicy::Manual)
+			.try_into()?;
+
+		let searcher = reader.searcher();
+
+		// so immediate complication to deal with; need to specify the fields to search if the user (lmao as if) doesn't specify any. we... techncially want to search _every_thing. or, at least every string thing? idfk. all strings makes sense i guess?
+		let fields = schema
+			.fields()
+			.filter(|(_field, entry)| matches!(entry.field_type(), FieldType::Str(_)))
+			.map(|(field, _entry)| field)
+			.collect::<Vec<_>>();
+
+		tracing::info!("tryna query");
+		let query_parser = QueryParser::for_index(&index, fields);
+		let query = query_parser.parse_query("summon")?;
+
+		let top_docs = searcher.search(&query, &TopDocs::with_limit(100))?;
+
+		for (score, doc_addresss) in top_docs {
+			let doc = searcher.doc(doc_addresss)?;
+			let id = doc
+				.get_first(schema.get_field("id").unwrap())
+				.unwrap()
+				.as_u64()
+				.unwrap();
+			let row = sheet.row(id.try_into().unwrap()).unwrap();
+			tracing::debug!(
+				"{score}: {:?}",
+				row.field(0).unwrap().as_string().unwrap().to_string()
+			);
+		}
+
 		tracing::info!("bailing before ingest");
 		return Ok(());
 	}
 
 	let mut writer = index.writer(50 * 1024 * 1024)?;
 
-	// test with single row
-	// TODO: need an iterator of some kind for this.
-	tracing::info!("writing row");
-	let row = sheet.row(25831)?;
-	let mut document = Document::new();
-	for (index, column) in sheet.columns()?.iter().enumerate() {
-		let field = schema.get_field(&column.offset().to_string()).unwrap();
-		// TODO: this would really value .field(impl intocolumn) or similar
-		let value = row.field(index)?;
-		// TODO: this feels pretty repetetive given the column kind schema build - is it avoidable or nah?
-		use Field as F;
-		match value {
-			F::String(value) => document.add_text(field, value),
+	// TODO: this is nightmare fuel, break up. a lot.
+	// TODO: if there's any failures at all (i.e. iw read errors) during ingestion, the writer should be rolled back to ensure a theoretical retry is able to work on a clean deck.
+	tracing::debug!("ingesting {index_name}");
+	for row in sheet.iter() {
+		let mut document = Document::new();
 
-			F::I8(value) => document.add_i64(field, i64::from(value)),
-			F::I16(value) => document.add_i64(field, i64::from(value)),
-			F::I32(value) => document.add_i64(field, i64::from(value)),
-			F::I64(value) => document.add_i64(field, value),
+		document.add_u64(schema.get_field("id").unwrap(), (*row.row_id()).into());
 
-			F::U8(value) => document.add_u64(field, u64::from(value)),
-			F::U16(value) => document.add_u64(field, u64::from(value)),
-			F::U32(value) => document.add_u64(field, u64::from(value)),
-			F::U64(value) => document.add_u64(field, value),
+		for (index, column) in sheet.columns()?.iter().enumerate() {
+			let field = schema.get_field(&column.offset().to_string()).unwrap();
+			// TODO: this would really value .field(impl intocolumn) or similar
+			let value = row.field(index)?;
+			// TODO: this feels pretty repetetive given the column kind schema build - is it avoidable or nah?
+			use Field as F;
+			match value {
+				F::String(value) => document.add_text(field, value),
 
-			F::F32(value) => document.add_f64(field, f64::from(value)),
+				F::I8(value) => document.add_i64(field, i64::from(value)),
+				F::I16(value) => document.add_i64(field, i64::from(value)),
+				F::I32(value) => document.add_i64(field, i64::from(value)),
+				F::I64(value) => document.add_i64(field, value),
 
-			F::Bool(value) => document.add_u64(field, u64::from(value)),
+				F::U8(value) => document.add_u64(field, u64::from(value)),
+				F::U16(value) => document.add_u64(field, u64::from(value)),
+				F::U32(value) => document.add_u64(field, u64::from(value)),
+				F::U64(value) => document.add_u64(field, value),
+
+				F::F32(value) => document.add_f64(field, f64::from(value)),
+
+				F::Bool(value) => document.add_u64(field, u64::from(value)),
+			}
 		}
-	}
 
-	// this can block; which suggests to me that at minimum, ingesting should be done on the side.
-	writer.add_document(document)?;
+		// this can block; which suggests to me that at minimum, ingesting should be done on the side.
+		writer.add_document(document)?;
+	}
 
 	writer.commit()?;
 
@@ -104,6 +146,7 @@ pub fn temp_test_search(excel: &Excel) -> Result<()> {
 fn build_sheet_schema(sheet: &Sheet<&str>) -> Result<Schema> {
 	let mut schema_builder = Schema::builder();
 
+	// TODO: need rowid and subrowid
 	schema_builder.add_u64_field("id", schema::STORED);
 	for column in sheet.columns()? {
 		// TODO: technically speaking, using offset means both offset-ordered and column-ordered will work. right?
