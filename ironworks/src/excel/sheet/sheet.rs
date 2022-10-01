7e53f33b389765aa3466581e31877c55d627d2d0
ackwell <saxon@ackwell.com.au>
2022-10-01 17:05:47 +1000

    Move language fallback handling to page lookup logic

--- ironworks/src/excel/sheet/sheet.rs
+++ ironworks/src/excel/sheet/sheet.rs
@@ -121,21 +121,8 @@ impl<'i, S: SheetMetadata> Sheet<'i, S> {
 			return Err(row_not_found());
 		}
 
-		// Get the language to load, or NONE if the language is not supported by this sheet.
-		// TODO: Should an explicit language request fail hard on miss?
-		let requested_language = options.language.unwrap_or(self.default_language);
-		let language = *header
-			.languages()
-			.get(&requested_language)
-			.or_else(|| header.languages().get(&LANGUAGE_NONE))
-			// TODO: Should this be Invalid or NotFound?
-			// TODO: Should we have an explicit ErrorValue for language?
-			.ok_or_else(|| {
-				Error::NotFound(ErrorValue::Other(format!("language {requested_language}")))
-			})?;
-
 		// Try to read in the page for the requested (sub)row.
-		let page = self.page(row_id, subrow_id, language)?;
+		let page = self.page(row_id, subrow_id, options.language)?;
 
 		let data = match header.kind() {
 			exh::SheetKind::Subrows => page.subrow_data(row_id, subrow_id),
@@ -156,9 +143,28 @@ impl<'i, S: SheetMetadata> Sheet<'i, S> {
 	}
 
 	// TODO: not a fan of the subrow id in this
-	fn page(&self, row_id: u32, subrow_id: u16, language: u8) -> Result<Arc<exd::ExcelData>> {
-		let start_id = self
-			.header()?
+	fn page(
+		&self,
+		row_id: u32,
+		subrow_id: u16,
+		language: Option<u8>,
+	) -> Result<Arc<exd::ExcelData>> {
+		let header = self.header()?;
+
+		// Get the language to load, or NONE if the language is not supported by this sheet.
+		// TODO: Should an explicit language request fail hard on miss?
+		let requested_language = language.unwrap_or(self.default_language);
+		let language = *header
+			.languages()
+			.get(&requested_language)
+			.or_else(|| header.languages().get(&LANGUAGE_NONE))
+			// TODO: Should this be Invalid or NotFound?
+			// TODO: Should we have an explicit ErrorValue for language?
+			.ok_or_else(|| {
+				Error::NotFound(ErrorValue::Other(format!("language {requested_language}")))
+			})?;
+
+		let start_id = header
 			.pages()
 			.iter()
 			.find(|page| page.start_id() <= row_id && page.start_id() + page.row_count() > row_id)
@@ -171,13 +177,10 @@ impl<'i, S: SheetMetadata> Sheet<'i, S> {
 			})?
 			.start_id();
 
+		// Try to read in the page for the requested (sub)row.
 		self.cache
 			.pages
 			.try_get_or_insert((start_id, language), || {
-				eprintln!(
-					"READ EXD: name:{} startid:{start_id} lang:{language}",
-					self.sheet_metadata.name()
-				);
 				let path = self
 					.mapper
 					.exd(&self.sheet_metadata.name(), start_id, language);
@@ -207,23 +210,16 @@ impl<'i, S: SheetMetadata> SheetIterator<'i, S> {
 	}
 }
 
-impl<S: SheetMetadata> Iterator for SheetIterator<'_, S>
-where
-	S::Row: Debug,
-{
+impl<S: SheetMetadata> Iterator for SheetIterator<'_, S> {
 	type Item = S::Row;
 
 	fn next(&mut self) -> Option<Self::Item> {
 		// TODO: both the .page and .subrow calls should have some means to utilise an iter-wide lang override
-		// TODO: need to fall back to 0-lang a-la how main row logic currently functions
 
 		let subrow_count = match self.subrow_count {
 			Some(v) => v,
 			None => {
-				let page = self
-					.sheet
-					.page(self.row_id, self.subrow_id, self.sheet.default_language)
-					.ok()?;
+				let page = self.sheet.page(self.row_id, self.subrow_id, None).ok()?;
 				let subrow_count = page.subrow_count(self.row_id).ok()?;
 				*self.subrow_count.insert(subrow_count)
 			}
