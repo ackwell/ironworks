fcac99e7b96ed015eec828ffcf0229d6ba745b39
ackwell <saxon@ackwell.com.au>
2022-10-24 22:22:33 +1100

    Clean up error handling a bit

--- ironworks/src/file/patch/zipatch.rs
+++ ironworks/src/file/patch/zipatch.rs
@@ -20,6 +20,7 @@ pub struct ZiPatch {
 }
 
 impl ZiPatch {
+	// note that it iters results because reads can fail
 	pub fn todo_name_me_iterate_chunks(&self) -> ChunkIterator {
 		ChunkIterator::new(self.stream.clone())
 	}
@@ -57,31 +58,21 @@ impl ChunkIterator {
 			complete: false,
 		}
 	}
-}
 
-impl Iterator for ChunkIterator {
-	type Item = Chunk;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		if self.complete {
-			return None;
-		}
-
+	fn read_chunk(&mut self) -> Result<Chunk> {
 		let mut handle = self.stream.lock().unwrap();
 
 		// Seek to last known offset - in a tight loop this is effectively a noop,
 		// but need to make sure if there's stuff jumping around.
 		// TODO: lots of jumping around would be catastrophic for read performance - it'd be nice to be able to request something cloneable, so i.e. file handles could be cloned between chunk iterators, rather than trying to share access to a single one - but i'm not sure how to mode that without major refactors.
-		handle
-			.seek(SeekFrom::Start(self.offset))
-			.expect("TODO this shouldn't happen, right? what do");
+		handle.seek(SeekFrom::Start(self.offset))?;
 
-		let size = u32::read_be(&mut *handle).expect("e");
+		let size = u32::read_be(&mut *handle)?;
 
 		// Chunk reading will need to use the stream, drop our handle on it.
 		drop(handle);
 
-		let chunk = Chunk::read(self.stream.clone()).expect("e");
+		let chunk = Chunk::read(self.stream.clone())?;
 
 		// Update iterator offset to the start of the next chunk. `size` only represents
 		// the size of the chunk data itself, so the +12 is to account for the other
@@ -90,7 +81,21 @@ impl Iterator for ChunkIterator {
 
 		// TODO: check the hash? is it worth it? I'd need to relock the stream for that...
 
-		if let Chunk::EndOfFile = chunk {
+		Ok(chunk)
+	}
+}
+
+impl Iterator for ChunkIterator {
+	type Item = Result<Chunk>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.complete {
+			return None;
+		}
+
+		let chunk = self.read_chunk();
+
+		if let Ok(Chunk::EndOfFile) = chunk {
 			self.complete = true;
 		}
 
