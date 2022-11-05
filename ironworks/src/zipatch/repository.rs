469220c3133871891b6f5ee41fa17caf70f715a6
ackwell <saxon@ackwell.com.au>
2022-11-05 18:11:33 +1100

    Add path -> repository coercion

--- ironworks/src/zipatch/repository.rs
+++ ironworks/src/zipatch/repository.rs
@@ -1,4 +1,10 @@
-use std::path::PathBuf;
+use std::{
+	cmp::Ordering,
+	fs,
+	path::{Path, PathBuf},
+};
+
+use crate::error::Result;
 
 #[derive(Debug)]
 pub struct PatchRepository {
@@ -7,5 +13,59 @@ pub struct PatchRepository {
 }
 
 impl PatchRepository {
-	// TODO: from_dir, plus From<PathBuf> that calls it
+	pub fn from_directory(repository_path: &Path) -> Result<Self> {
+		let mut patches = fs::read_dir(repository_path)?
+			.filter_map(|entry| {
+				let patch_path = match entry {
+					Err(err) => return Some(Err(err)),
+					Ok(entry) => entry.path(),
+				};
+
+				let extension = patch_path.extension().and_then(|osstr| osstr.to_str());
+				match patch_path.is_file() && extension == Some("patch") {
+					// TODO: should this error if the string conversion fails? atm it just ->None's
+					true => patch_path
+						.file_stem()
+						.and_then(|osstr| osstr.to_str())
+						.map(|str| Ok(str.to_string())),
+
+					false => None,
+				}
+			})
+			.collect::<Result<Vec<_>, _>>()?;
+
+		patches.sort_unstable_by(|a, b| sort_patches(a, b));
+
+		Ok(Self {
+			base_directory: repository_path.to_owned(),
+			patches,
+		})
+	}
+}
+
+fn sort_patches(a: &str, b: &str) -> Ordering {
+	match a[1..].cmp(&b[1..]) {
+		// The prefix "type" character is only ever [D]IFF or [H]IST - HIST always sorts first.
+		Ordering::Equal => a[0..1].cmp(&b[0..1]).reverse(),
+
+		// The primary "version" portion of the patch name is string-sortable (Y.M.D.P.Rp, where [P]art, [R]evision, [p]art-but-for-HISTs).
+		order => order,
+	}
+}
+
+// Dumb trait helper because TryFrom's blanket impl prevents me using it.
+pub trait IntoPatchRepository {
+	fn into_repository(self) -> Result<PatchRepository>;
+}
+
+impl IntoPatchRepository for PatchRepository {
+	fn into_repository(self) -> Result<PatchRepository> {
+		Ok(self)
+	}
+}
+
+impl<P: AsRef<Path>> IntoPatchRepository for P {
+	fn into_repository(self) -> Result<PatchRepository> {
+		PatchRepository::from_directory(self.as_ref())
+	}
 }
--- ironworks/src/zipatch/zipatch.rs
+++ ironworks/src/zipatch/zipatch.rs
@@ -5,7 +5,11 @@ use std::{
 
 use crate::error::Result;
 
-use super::{lookup::PatchLookup, repository::PatchRepository, version::Version};
+use super::{
+	lookup::PatchLookup,
+	repository::{IntoPatchRepository, PatchRepository},
+	version::Version,
+};
 
 #[derive(Debug)]
 pub struct ZiPatch {
@@ -15,9 +19,6 @@ pub struct ZiPatch {
 }
 
 impl ZiPatch {
-	// TODO: API. This should probably take an initial list of patches, grouped by repository or similar, and ordered by their requisite application/dependency order. alternative would be to use a self-building pattern i.e. .add/with_repository
-	// i'm tempted to say Vec<String> should be a struct, instead, with "path to the patch folder" and "patches" as seperate concepts. the alternative is to accept just pathbufs anyway and encode xiv patch sorting logic; which is honestly tempting. if i do take that route, a theoreticaly api evolution could change that to `Into<Repository>` wherein a path is an autosorted repository and other options can define their own impl
-	// the u8 is probably not a go on the public api honestly. if i do the builder pattern i can probably Into... and accept both, but for average-user config, passing the repository id is JANK.
 	pub fn new() -> Self {
 		Self {
 			repositories: HashMap::default(),
@@ -25,14 +26,15 @@ impl ZiPatch {
 		}
 	}
 
-	#[must_use]
-	pub fn with_repository(mut self, id: u8, repository: PatchRepository) -> Self {
-		self.add_repository(id, repository);
-		self
+	pub fn with_repository(mut self, id: u8, repository: impl IntoPatchRepository) -> Result<Self> {
+		self.add_repository(id, repository)?;
+		Ok(self)
 	}
 
-	pub fn add_repository(&mut self, id: u8, repository: PatchRepository) {
-		self.repositories.insert(id, repository.into());
+	pub fn add_repository(&mut self, id: u8, repository: impl IntoPatchRepository) -> Result<()> {
+		let repository = repository.into_repository()?;
+		self.repositories.insert(id, Arc::new(repository));
+		Ok(())
 	}
 
 	// TODO: API. Assuming going with the latter from new()'s comment, this should accept some "version" concept that declares the patch point for each repository.
