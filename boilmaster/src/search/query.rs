7191017a01dad2449031a32ee805b6c3462d240c
ackwell <saxon@ackwell.com.au>
2022-12-16 00:49:37 +1100

    Add some documentation to query

--- boilmaster/src/search/query.rs
+++ boilmaster/src/search/query.rs
@@ -19,6 +19,8 @@ pub enum Occur {
 
 #[derive(Debug)]
 pub struct Leaf {
+	/// Column offset this leaf targets.
+	// TODO: this struct targets post-normalised data, so i'm acting under the assumption that the normalisation process will turn non-targeted queries into a group of queries targeting specific fields.
 	pub offset: u32,
 	pub operation: Operation,
 }
@@ -28,16 +30,23 @@ pub enum Operation {
 	Relation(Relation),
 
 	Equal(Value),
+	// TODO: all the other relevant leaf operations. will need both further math operations, as well as ranges and string ops (given i'm using this instead of generic string param)
 }
 
 #[derive(Debug)]
 pub struct Relation {
+	/// Target sheet of this relation
 	pub target: String,
+	/// Additional conditions on the parent sheet required for this relation to be applicable for a row.
 	pub condition: Option<Box<Node>>,
+	/// Query to be executed on the target sheet's index.
 	pub query: Box<Node>,
 }
 
+// TODO: this can probably be used on both sides of normalisation
 #[derive(Debug)]
 pub enum Value {
+	/// Represents any positive integer
 	U64(u64),
+	// TODO: other value types - we'll need something for negative ints (i64?), floats (f64?), and strings (String?) at minimum
 }
