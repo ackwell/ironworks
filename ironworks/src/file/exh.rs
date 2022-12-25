aa9643823b86a0f6e00764ed139d56ed6bdc9791
ackwell <saxon@ackwell.com.au>
2022-12-26 01:42:25 +1100

    Fix sheet iterator to handle sparse pages

--- ironworks/src/file/exh.rs
+++ ironworks/src/file/exh.rs
@@ -134,7 +134,7 @@ pub enum ColumnKind {
 
 /// Metadata for a single sheet data page.
 #[binread]
-#[derive(Debug, CopyGetters)]
+#[derive(Debug, Clone, Copy, CopyGetters)]
 #[br(big)]
 pub struct PageDefinition {
 	/// The first ID contained within the page.
