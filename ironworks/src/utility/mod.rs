01c580ee80e68a1a313b90874a751c8cd36156de
ackwell <saxon@ackwell.com.au>
2024-08-02 21:51:54 +1000

    Remove OptionCache

--- ironworks/src/utility/mod.rs
+++ ironworks/src/utility/mod.rs
@@ -1,9 +1,7 @@
 mod hash_map_cache;
-mod option_cache;
 mod take_seekable;
 
 pub use {
 	hash_map_cache::{HashMapCache, HashMapCacheExt},
-	option_cache::{OptionCache, OptionCacheExt},
 	take_seekable::{TakeSeekable, TakeSeekableExt},
 };
