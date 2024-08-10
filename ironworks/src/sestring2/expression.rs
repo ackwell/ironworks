163dc4197c757309425a17d18b463078ceead776
ackwell <saxon@ackwell.com.au>
2024-08-10 21:35:20 +1000

    Add packed u32 expression reading

--- ironworks/src/sestring2/expression.rs
+++ ironworks/src/sestring2/expression.rs
@@ -11,9 +11,24 @@ impl Expression {
 	pub fn read(cursor: &mut SliceCursor) -> Result<Self, Error> {
 		let kind = cursor.next()?;
 
-		match kind {
-			value @ 0x01..=0xCF => Ok(Self::U32(u32::from(value - 1))),
+		let expression = match kind {
+			value @ 0x01..=0xCF => Self::U32(u32::from(value - 1)),
+			kind @ 0xF0..=0xFE => Self::U32(read_packed_u32(cursor, kind)?),
 			other => todo!("unhandled expression kind {other:?}"),
-		}
+		};
+
+		Ok(expression)
 	}
+}
+
+fn read_packed_u32(cursor: &mut SliceCursor, kind: u8) -> Result<u32, Error> {
+	let flags = (kind + 1) & 0b1111;
+	let mut bytes = [0; 4];
+	for i in (0..=3).rev() {
+		if (flags & (1 << i)) == 0 {
+			continue;
+		}
+		bytes[i] = cursor.next()?;
+	}
+	Ok(u32::from_le_bytes(bytes))
 }
