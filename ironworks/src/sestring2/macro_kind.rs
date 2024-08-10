6cc09e36fa2fc376a8cff0c6ad30b2ffa21eac5e
ackwell <saxon@ackwell.com.au>
2024-08-10 16:08:19 +1000

    Add remaining macro kinds

--- ironworks/src/sestring2/macro_kind.rs
+++ ironworks/src/sestring2/macro_kind.rs
@@ -1,17 +1,67 @@
+use num_enum::FromPrimitive;
+
+// Based on Lumina implementation as of 2024-08-10
+// https://github.com/NotAdam/Lumina/blob/e99b736f18b0962a28d326cd290490b121ed679b/src/Lumina/Text/Payloads/MacroCode.cs
+
 #[non_exhaustive]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, FromPrimitive)]
+#[repr(u8)]
 pub enum MacroKind {
-	NewLine,
+	SetResetTime = 0x06,
+	SetTime = 0x07,
+	If = 0x08,
+	Switch = 0x09,
+	PcName = 0x0A,
+	IfPcGender = 0x0B,
+	IfPcName = 0x0C,
+	Josa = 0x0D,
+	Josaro = 0x0E,
+	IfSelf = 0x0F,
+	NewLine = 0x10,
+	Wait = 0x11,
+	Icon = 0x12,
+	Color = 0x13,
+	EdgeColor = 0x14,
+	ShafowColor = 0x15,
+	SoftHyphen = 0x16,
+	Key = 0x17,
+	Scale = 0x18,
+	Bold = 0x19,
+	Italic = 0x1A,
+	Edge = 0x1B,
+	Shadow = 0x1C,
+	NonBreakingSpace = 0x1D,
+	Icon2 = 0x1E,
+	Hyphen = 0x1F,
+	Num = 0x20,
+	Hex = 0x21,
+	Kilo = 0x22,
+	Byte = 0x23,
+	Sec = 0x24,
+	Time = 0x25,
+	Float = 0x26,
+	Link = 0x27,
+	Sheet = 0x28,
+	String = 0x29,
+	Caps = 0x2A,
+	Head = 0x2B,
+	Split = 0x2C,
+	HeadAll = 0x2D,
+	Fixed = 0x2E,
+	Lower = 0x2F,
+	JaNoun = 0x30,
+	EnNoun = 0x31,
+	DeNoun = 0x32,
+	FrNoun = 0x33,
+	ChNoun = 0x34,
+	LowerHead = 0x40,
+	ColorType = 0x48,
+	EdgeColorType = 0x49,
+	Digit = 0x50,
+	Ordinal = 0x51,
+	Sound = 0x60,
+	LevelPos = 0x61,
 
+	#[num_enum(catch_all)]
 	Unknown(u8),
-}
-
-impl From<u8> for MacroKind {
-	fn from(value: u8) -> Self {
-		match value {
-			0x10 => Self::NewLine,
-
-			other => Self::Unknown(other),
-		}
-	}
 }
